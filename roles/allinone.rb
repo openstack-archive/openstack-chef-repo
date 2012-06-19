c48254ea786a64a16a6de80ac285caa3d4b1e769
mattray <matthewhray@gmail.com>
2012-06-18 21:58:13 -0500

    removed monitoring recipes and focusing on core OpenStack

--- roles/allinone.rb
+++ roles/allinone.rb
@@ -1,9 +1,6 @@
 name "allinone"
 description "This will create an all-in-one Openstack cluster"
 run_list(
-  "role[single-controller]",
-  "role[single-compute]",
-  "role[collectd-client]",
-  "role[collectd-server]",
-  "role[graphite]"
+  "role[single-controller]"
+  # "role[single-compute]"
 )
