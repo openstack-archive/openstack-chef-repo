66ba16bb58afd290b493e5ce5fd4a3ed0bd48f00
mattray <matthewhray@gmail.com>
2012-08-14 14:32:00 -0500

    removing cookbooks, all in on librarian-chef now

--- cookbooks/osops-utils/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Rackspace Hosting"
-maintainer_email "osops@lists.launchpad.net"
-license          "Apache 2.0"
-description      "Installs/Configures osops-utils"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.3"
