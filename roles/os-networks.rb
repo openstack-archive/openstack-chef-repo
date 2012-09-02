b44fb3b4184217e3c29d1bd21c8345051cc103cc
mattray <matthewhray@gmail.com>
2012-09-02 14:59:24 -0500

    working with private networks

--- roles/os-networks.rb
+++ roles/os-networks.rb
@@ -5,33 +5,32 @@ override_attributes(
   "osops_networks" => {
     "public" => "10.0.111.0/24",
     "management" => "10.0.200.0/24",
-    "nova" => "10.0.111.0/24"
+    "nova" => "10.0.200.0/24"
   },
   "nova" => {
     "network" => {
-      "fixed_range" => "10.0.111.128/25"
+      "fixed_range" => "192.168.100.0/24"
     },
     "networks" => [
       {
         "label" => "public",
-        "ipv4_cidr" => "10.0.111.128/25",
-        "num_networks" => "1",
-        "network_size" => "128",
-        "bridge" => "br100",
-        "bridge_dev" => "eth0.100",
-        "dns1" => "8.8.8.8",
-        "dns2" => "8.8.4.4"
-      },
-      {
-        "label" => "private",
-        "ipv4_cidr" => "10.0.222.0/24",
+        "ipv4_cidr" => "192.168.100.0/24",
         "num_networks" => "1",
         "network_size" => "255",
-        "bridge" => "br101",
-        "bridge_dev" => "eth0.101",
+        "bridge" => "br100",
+        "bridge_dev" => "eth0",
         "dns1" => "8.8.8.8",
         "dns2" => "8.8.4.4"
       }
+      # {
+      #   "label" => "private",
+      #   "ipv4_cidr" => "192.168.200.0/24",
+      #   "num_networks" => "1",
+      #   "network_size" => "255",
+      #   "bridge" => "br200",
+      #   "bridge_dev" => "eth1"
+      # }
+
     ]
   }
   )
