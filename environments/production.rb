3c57affb23faedbaf60e5b2cd6700742ac74a262
mattray <matthewhray@gmail.com>
2012-10-04 21:58:33 -0500

    single nic network

--- environments/production.rb
+++ environments/production.rb
@@ -2,18 +2,27 @@ name "production"
 description "Defines the network and database settings you're going to use with OpenStack. The networks will be used in the libraries provided by the osops-utils cookbook. This example is for FlatDHCP with 2 physical networks."
 
 override_attributes(
+  "glance" => {
+    "image_upload" => true,
+    "images" => ["precise","cirros"],
+    "image" => {
+      "cirros" => "http://hypnotoad/cirros-0.3.0-x86_64-uec.tar.gz",
+      "precise" => "http://hypnotoad/precise-server-cloudimg-amd64.tar.gz"
+    }
+  },
   "mysql" => {
     "allow_remote_root" => true,
     "root_network_acl" => "%"
   },
   "osops_networks" => {
     "public" => "10.0.111.0/24",
-    "management" => "10.0.200.0/24",
-    "nova" => "10.0.200.0/24"
+    "management" => "10.0.111.0/24",
+    "nova" => "10.0.111.0/24"
   },
   "nova" => {
     "network" => {
-      "fixed_range" => "192.168.100.0/24"
+      "fixed_range" => "192.168.100.0/24",
+      "public_interface" => "eth0"
     },
     "networks" => [
       {
