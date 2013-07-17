1d69ea3d745d4350b4650bb091c16ece6e359297
John Tran <jhtran@att.com>
2013-07-17 21:36:10 +0000

    os-ops-messaging points to missing default recipe
    Fixes bug 1202400
    
    Change-Id: I97990591437d23b5142470f0300fafcbb8bdd56d

--- roles/os-ops-messaging.rb
+++ roles/os-ops-messaging.rb
@@ -2,5 +2,5 @@ name "os-ops-messaging"
 description "Currently RabbitMQ Server (non-ha)"
 run_list(
   "role[os-base]",
-  "recipe[openstack-ops-messaging]"
+  "recipe[openstack-ops-messaging::server]"
   )
