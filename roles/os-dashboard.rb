8836ee0b9dc8b186a3da9620183dac1c721bd5d2
John Tran <jhtran@att.com>
2013-07-28 18:04:00 +0000

    update roles that call db recipe
    They all should be pointing to the updated openstack-ops-database::openstack-db
    recipe
    
    Change-Id: I33f243db03ac5f408bb12de788c25832e4e373aa

--- roles/os-dashboard.rb
+++ roles/os-dashboard.rb
@@ -2,5 +2,6 @@ name "os-dashboard"
 description "Horizon server"
 run_list(
   "role[os-base]",
+  "recipe[openstack-ops-database::openstack-db]",
   "recipe[openstack-dashboard::server]"
   )
--- roles/os-image-registry.rb
+++ roles/os-image-registry.rb
@@ -2,6 +2,6 @@ name "os-image-registry"
 description "Glance Registry service"
 run_list(
   "role[os-base]",
+  "recipe[openstack-ops-database::openstack-db]",
   "recipe[openstack-image::registry]"
   )
-
