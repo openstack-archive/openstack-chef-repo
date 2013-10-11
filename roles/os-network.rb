668aa6462f24b9482075b5830a5611ae75add7bd
Matt Ray <matthewhray@gmail.com>
2013-10-11 17:05:13 -0500

    Enable all of the services required for networking.
    This will probably get refactored later into more roles.
    
    Change-Id: I303edfc8db1237acf507b0d5fe886dc66e0baeee

--- roles/os-network.rb
+++ roles/os-network.rb
@@ -2,5 +2,9 @@ name "os-network"
 description "Configures OpenStack networking, managed by attribute for either nova-network or quantum"
 run_list(
   "role[os-base]",
-  "recipe[openstack-network::common]"
+  "recipe[openstack-network::identity_registration]",
+  "recipe[openstack-network::server]",
+  "recipe[openstack-network::openvswitch]",
+  "recipe[openstack-network::l3_agent]",
+  "recipe[openstack-network::dhcp_agent]"
   )
