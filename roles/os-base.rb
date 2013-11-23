5af3a32e8c6ab6c45e2bc7c05c27f30293f4dadf
Matt Ray <matthewhray@gmail.com>
2013-11-22 22:47:19 -0600

    Add common::set_endpoints_by_interface & common::sysctl to os-base role
    These recipes are part of the openstack-common cookbook and are managed
    completely by optional attributes, so they do not affect the existing behavior.
    
    Change-Id: I67b97dfe0fc467656368c2a52d5f9e3198975c1a

--- roles/os-base.rb
+++ roles/os-base.rb
@@ -2,5 +2,7 @@ name "os-base"
 description "OpenStack Base role"
 run_list(
   "recipe[openstack-common]",
-  "recipe[openstack-common::logging]"
+  "recipe[openstack-common::logging]",
+  "recipe[openstack-common::set_endpoints_by_interface]",
+  "recipe[openstack-common::sysctl]"
 )
