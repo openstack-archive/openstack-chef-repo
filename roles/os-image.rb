76cd43d73a8947ca8e39ddd5fb745ea6407021db
Matt Ray <matthewhray@gmail.com>
2013-12-17 15:44:23 -0600

    Include 'role[os-image-upload]' to upload images
    The os-image role is a roll-up role, so if any attributes for uploading images
    are included, they'll get uploaded. Also included replacing double-quotes with
    single quotes, per rubocop's complaints.
    
    Change-Id: I38b53033dfb56ae8f8e1256cbb6c04208abb0b26

--- roles/os-image.rb
+++ roles/os-image.rb
@@ -1,7 +1,8 @@
-name "os-image"
-description "Roll-up role for Glance."
+name 'os-image'
+description 'Roll-up role for Glance.'
 run_list(
-  "recipe[openstack-image::identity_registration]",
-  "role[os-image-api]",
-  "role[os-image-registry]"
+  'recipe[openstack-image::identity_registration]',
+  'role[os-image-api]',
+  'role[os-image-registry]',
+  'role[os-image-upload]'
   )
