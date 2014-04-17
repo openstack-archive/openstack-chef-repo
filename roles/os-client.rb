4a8b857428ad08e563cdf4a4dc94b85f585ddc19
Mark Vanderwiel <vanderwl@us.ibm.com>
2014-04-17 16:06:45 -0500

    Add client recipes to role
    Looks like I missed a couple.
    
    Change-Id: I26d5009b3bfe1777a4934808c01d6c3f520d1e12
    Implements: blueprint openstack-client

--- roles/os-client.rb
+++ roles/os-client.rb
@@ -3,10 +3,12 @@ description 'Roll-up role for client interfaces.'
 run_list(
   'role[os-base]',
   'recipe[openstack-common::client]',
+  'recipe[openstack-block-storage::client]',
+  'recipe[openstack-compute::client]',
   'recipe[openstack-identity::client]',
   'recipe[openstack-image::client]',
   'recipe[openstack-network::client]',
-  'recipe[openstack-compute::client]',
+  'recipe[openstack-object-storage::client]',
   'recipe[openstack-orchestration::client]',
-  'recipe[openstack-block-storage::client]'
+  'recipe[openstack-telemetry::client]'
   )
