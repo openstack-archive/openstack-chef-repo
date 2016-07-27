f9ffdfa11fe302afcddfa84aa2f4274739fc9515
Samuel Cassiba <s@cassiba.com>
2016-07-26 21:17:38 -0700

    Enables Ubuntu Xenial release (16.04)
    - replaces ubuntu14 with ubuntu16
    - style fixes to make rubocop happy
    - converts smoke tests to python-openstackclient
    - tweaks memory limits for multi_node
    - corrects ethernet interface naming
    - prunes misleading pre-provisioning environment files
    - uses atlas for vagrant images
    - makes the destroy_all recipe work again
    
    Change-Id: I79aa5f793614f5430c613dc0b2fc241434fde1f9
    Implements: blueprint newton-xenial

--- site-cookbooks/provisioning/recipes/vagrant_linux.rb
+++ site-cookbooks/provisioning/recipes/vagrant_linux.rb
@@ -1,17 +1,9 @@
 require 'chef/provisioning/vagrant_driver'
 
-vagrant_box 'centos7.2' do
-  url 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_centos-7.2_chef-provisionerless.box'
-end
+os = 'ubuntu/xenial64'
+os = 'centos/7' if ENV['REPO_OS'].to_s.include?('centos')
 
-vagrant_box 'ubuntu14' do
-  url 'http://opscode-vm-bento.s3.amazonaws.com/vagrant/virtualbox/opscode_ubuntu-14.04_chef-provisionerless.box'
-end
-
-os = 'ubuntu14'
-os = 'centos7.2' if ENV['REPO_OS'].to_s.include?('centos')
-
-with_driver "vagrant:#{File.dirname(__FILE__)}/vms"
+with_driver 'vagrant'
 with_machine_options vagrant_options: {
   'vm.box' => os
 }
