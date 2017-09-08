962633f25ae7b2ccdfb7cb6801efd0b95295f697
Roger Luethi <rl@patchworkscience.org>
2017-09-08 14:15:19 +0200

    Add support for kitchen testing
    This changeset adds support for testing openstack-chef-repo with kitchen
    and tempest.
    
    Out of 63 tests, 33 pass and 30 are skipped, same as with current CI
    tests.
    
    The recipe, run_tempest in openstack-integration-test, is called at the
    end of the installation phase.
    
    In addition, a tempest run can be executed manually with
    "kitchen verify".
    
    Depends-On: I98706b1fc2c65a2359ca9b117a9491c0569514bd
    Change-Id: I90c4ab627e3834ba9fd5791c425320e9536f7826

---
+++ test/tempest/default/default_test.rb
@@ -0,0 +1,14 @@
+# # encoding: utf-8
+
+# Inspec test for openstack-chef-repo
+
+# The Inspec reference, with examples and extensive documentation, can be
+# found at http://inspec.io/docs/reference/resources/
+
+# There is no console output during runtime, check log file instead.
+describe command('sudo bash -c "cd /opt/tempest && ' \
+                 'sudo /opt/tempest-venv/tempest.sh | ' \
+                 'tee /root/inspec-tempest-$(date -Iminutes).log &&' \
+                 'exit \${PIPESTATUS[0]}"') do
+  its('exit_status') { should eq 0 }
+end
