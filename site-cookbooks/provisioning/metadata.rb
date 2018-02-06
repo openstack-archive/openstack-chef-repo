3df1fba78e8b688c50bdaa9e01cfff94958850f4
Samuel Cassiba <s@cassiba.com>
2018-02-06 06:49:04 -0800

    Cleaned up minimal role, deprecated provisioning cookbook
    - The minimal role contains a subset of allinone for use in integration. This
      allows to execute Tempest at the end of the Chef run.
    - Being that chef-provisioning-vagrant is no longer working as of ChefDK 2.x,
      the provisioning cookbook serves no functional purpose.
    
    Change-Id: I99a6de49640356c98adadf3237dc68383be06bb4
    Implements: blueprint modern-chef
    Depends-On: https://review.openstack.org/542612
    Depends-On: https://review.openstack.org/542613

--- site-cookbooks/provisioning/metadata.rb
+++
@@ -1,7 +0,0 @@
-name 'provisioning'
-maintainer 'openstack-chef'
-maintainer_email 'openstack-dev@lists.openstack.org'
-license 'Apache 2.0'
-description 'Helper cookbook for chef-provisioning'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.0'
